2ade5f70f4fbd877ac6eb5000789e86035083824
enerjhun relon <enerjhunrelon@gmail.com>
2022-11-13 10:17:13 +0800

    minor

--- src/js/nav_btns.js
+++ src/js/nav_btns.js
@@ -3,5 +3,13 @@ let menu = document.querySelector("#menu_opt_con")
 let menu_btn = document.querySelector("#menu")
 
 menu.onclick = ()=>{
+    open_drop_down(menu)
+    lo
+}
 
+
+
+
+function open_drop_down(el) {
+    el.style.display = "block"
 }
