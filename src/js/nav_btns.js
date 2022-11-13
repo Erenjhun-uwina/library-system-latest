5ccdc51bae58bbded27dc74c4cba9a09999085df
enerjhun relon <enerjhunrelon@gmail.com>
2022-11-13 13:29:26 +0800

    update

--- src/js/nav_btns.js
+++ src/js/nav_btns.js
@@ -1,15 +1,37 @@
 
 let menu = document.querySelector("#menu_opt_con")
-let menu_btn = document.querySelector("#menu")
+let menu_btn = document.querySelector(".menu")
 
-menu.onclick = ()=>{
-    open_drop_down(menu)
-    lo
+
+const dropdown = {
+    open: (el) => {
+        el.style.display = "block"
+        el.active = true
+    },
+    close: (el) => {
+        el.style.display = "none"
+        el.active = false
+    },
+    toggle: el => {
+        if (el.active) return dropdown.close(el)
+        dropdown.open(el)
+    }
+}
+
+
+menu_btn.onclick = (ev) => {
+    dropdown.toggle(menu)
+    ev.stopPropagation()
+    return false
+}
+
+window.onclick = ev => {
+
+    if (ev.target == menu_btn || !menu.active) return
+    dropdown.close(menu)
+    ev.stopPropagation()
+    return false
 }
 
 
 
-
-function open_drop_down(el) {
-    el.style.display = "block"
-}
